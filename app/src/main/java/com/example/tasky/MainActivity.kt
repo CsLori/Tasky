ee6dc3fbf6f951474c2e6401262dd9fdf2ef02cd
CsLori <csuhailori@gmail.com>
2024-09-28 23:34:53 +0100

    Added splash screen

--- app/src/main/java/com/example/tasky/MainActivity.kt
+++ app/src/main/java/com/example/tasky/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tasky.onboarding.onboarding.presentation.LoginScreen
 import com.example.tasky.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             AppTheme {
--- app/src/main/java/com/example/tasky/onboarding/onboarding/presentation/SplashScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.tasky.onboarding.onboarding.presentation
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SplashScreen() {
-
-}
