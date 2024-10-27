13dc87297fb8c8df3fe19986cbd347888f276fab
CsLori <csuhailori@gmail.com>
2024-10-27 17:39:04 +0000

    Refactor after review

--- app/src/main/java/com/example/tasky/core/presentation/Navigation.kt
+++ app/src/main/java/com/example/tasky/core/presentation/Navigation.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -54,7 +54,7 @@ fun Navigation() {
 
                     loginViewModel.checkUserSession()
 
-                    val sessionState by loginViewModel.sessionState.collectAsState()
+                    val sessionState by loginViewModel.sessionState.collectAsStateWithLifecycle()
 
                     LaunchedEffect(sessionState) {
                         sessionState?.let { state: LoginViewModel.SessionState ->
